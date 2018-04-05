5834a17616c5edbe0503d39a1440cef7d25a7e12
Melanie Strauss <52stme1bwi@hft-stuttgart.de>
2018-04-05 14:43:38 +0200

    Passwort eingebaut

--- app/src/main/java/com/example/a52tade1bwi/bhs2/MainActivity.java
+++ app/src/main/java/com/example/a52tade1bwi/bhs2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a52tade1bwi.bhs2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.goodiebag.pinview.Pinview;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Pinview pinview = (Pinview)findViewById(R.id.pinView);
+        pinview.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean b) {
+                Toast.makeText(MainActivity.this, ""+pinview.getValue(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
